5852eaac7e70a98a2bd0769e8a5eab0738c82fdc
Skarven1 <54530590+Skarven1@users.noreply.github.com>
2020-09-16 15:12:04 +0200

    Create initial version of http client (#1)
    * Wrote first test (failing)
    
    * Status code 200 green, status code 404 red
    
    * New failing test
    
    * implement responseHeader, add test for response body
    
    * Completed readresponsebody
    
    Co-authored-by: ghandeland <gabriel.handeland@gmail.com>
    Co-authored-by: Kaikka <kaileeto@gmail.com>

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -2,8 +2,14 @@ package no.kristiania.http;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HttpClient {
+    private int responseCode = 200;
+    private Map<String, String> responseHeaders = new HashMap<>();
+    private String responseBody = "";
+
     public HttpClient(final String host, int port, final String requestTarget) throws IOException {
 
         String request = "GET " + requestTarget + " HTTP/1.1\r\n"
@@ -14,13 +20,51 @@ public class HttpClient {
 
         socket.getOutputStream().write(request.getBytes());
 
+        String responseLine = readLine(socket);
+        System.out.println(responseLine);
+        String[] responseLineParts = responseLine.split(" ");
+        responseCode = Integer.parseInt(responseLineParts[1]);
+        String headerLine;
+
+        while(!(headerLine = readLine(socket)).isEmpty()){
+            int colonPos = headerLine.indexOf(':');
+            String headerName = headerLine.substring(0, colonPos);
+            String headerValue = headerLine.substring(colonPos + 1).trim();
+            responseHeaders.put(headerName, headerValue);
+        }
+        int contentLength = Integer.parseInt(getResponseHeader("Content-Length"));
+        for (int i = 0; i < contentLength; i++) {
+            responseBody+=(char)socket.getInputStream().read();
+        }
+    }
+
+    private String readLine(Socket socket) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
         int c;
         while((c = socket.getInputStream().read()) != -1) {
-            System.out.print((char) c);
+            if(c == '\r') {
+                socket.getInputStream().read();
+                break;
+            }
+            sb.append((char) c);
         }
+        return sb.toString();
     }
 
     public static void main(String[] args) throws IOException {
     new HttpClient("urlecho.appspot.com", 80, "/echo?body=Hello+World");
+    }
+
+    public int getResponseCode() {
+        return responseCode;
+    }
+
+    public String getResponseHeader(String headerName) {
+        return responseHeaders.get(headerName);
+    }
+
+    public String getResponseBody() {
+        return responseBody;
     }
 }
